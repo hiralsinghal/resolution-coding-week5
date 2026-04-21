7784eb1738d752aff6501280d1611e2a9a23f429
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-04-21 03:17:18 +0000

    adding functions loops in class lol

--- contact_book.c
+++ contact_book.c
@@ -8,6 +8,8 @@
 char *FILENAME = "contact_book_data.csv";
 
 int add_new_entry();
+int list_contacts();
+void print_contact();
 
 int main () {
     add_new_entry();
@@ -25,5 +27,63 @@ int add_new_entry() {
     char email[INPUT_LENGTH] = "input_here";
     char number[INPUT_LENGTH] = "number_here";
 
-    fprintf(fptr)
+    fprintf(fptr, "\n%s,%s,%s,%s", name, address, email, number);
+    fclose(fptr);
+
+    printf("\nSaved contact!\n\n");
+
+    return 0;
+}
+
+typedef struct Contact() {
+    char name[INPUT_LENGTH];
+    char address[INPUT_LENGTH];
+    char email[INPUT_LENGTH];
+    char number[INPUT_LENGTH];
+} Contact;
+
+Contact contact_array[100];
+
+int list_contacts() {
+    int contact_count = read_from_csv(contact_array);
+
+    for (int i = 0; i < contact_count; i++) {
+        print_contact(&contact_array[i]);
+    }
+
+    return 0;
+}
+
+void print_contact(const Contact *contact) {
+    printf("Name: %s\n", contact->name);
+    printf("Address: %s\n", contact->address);
+    printf("Email: %s\n", contact->email);
+    printf("\n");
+}
+
+int read_from_csv(Contact contact_array[]) {
+    FILE *fptr;
+    char buffer[(INPUT_LENGTH * 4 + 5)]
+    int i = 0;
+
+    fptr = fopen(FILENAME, "r");
+    if (fptr == NULL) {
+        printf("Could not open %s\n", FILENAME);
+        return 0;
+    }
+
+    fclose(fptr);
+
+    return i;
+}
+
+while (fgets(buffer, sizeof(buffer), fptr)) {
+    char *p = buffer;
+    while (*p == ' ' || *p == '\t' || *p == '\r' || *p == '\n') p++;
+    if (*p == '\0') continue;
+
+    char *new_line_pointer = strchr(buffer, '\n');
+    if (new_line_pointer) *new_line_pointer = '\0';
+
+    char *field = strtok(buffer, ",")
 }
